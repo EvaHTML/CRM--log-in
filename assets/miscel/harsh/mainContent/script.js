03e7238b4d53b4eeb0c1f28449968133340ed318
Harsh <84454566+Har99sh@users.noreply.github.com>
2022-06-23 20:20:03 +0200

    mezclando header y function y form

--- assets/miscel/harsh/mainContent/script.js
+++ assets/miscel/harsh/mainContent/script.js
@@ -1,17 +1,23 @@
 function createForm(){
     let adminForm = '';
-    adminForm += ' <article class="registerForm"><form> <label for="fname"> Full name: </label> <input type="text" id="fname" name="fname" value="">\
-                         <label for="fage"> Age: </label> <input type="number" id="fage" name="fage" value="">\
-                         <input type="submit" value="Submit"></input>\
-                         </form></article>'
+    adminForm +=  '<article class="userSignUpFormBox">'
+     adminForm += '<h3>Sign Up</h3><form>'                                                                                                           
+     adminForm += '<span class="userSignUpLabel"><label for="fName"></label><input type="text" id="fname" placeholder="First name"></span>'   
+     adminForm += '<span class="userSignUpLabel"><label for="fCompanyName"></label><input type="text" id="fCompanyName" placeholder="Company name"></span>'
+     adminForm += '<span class="userSignUpLabel"><label for="fEmail"></label><input type="email" id="fEmail" placeholder="E-mail"></span>'
+     adminForm += '<span class="userSignUpLabel"><label for="fTelephone"></label><input type="text" id="fTelephone" placeholder="Telephone"></span>'
+     adminForm += '<span class="userSignUpLabel"><label for="fUserNamd"></label><input type="text" id="fUserName" placeholder="User name"></span>'
+     adminForm += '<span class="userSignUpLabel"><label for="fPassword"></label><input type="password" id="fPassword" placeholder="Password"></span>'
+     adminForm += '<span class="userSignUpLabel submitButton"><input type="submit" value="Submit"></input></span>'
+     adminForm += '</form></article>'
+
     return adminForm;
 }
 
 function createFormToHTML(){
     let htmlForm = createForm();
-    document.querySelector("section").innerHTML = htmlForm;  
+    document.querySelector("section").innerHTML = "";
+    document.querySelector("content").innerHTML=htmlForm;
 }
 
-function pushRegisterDataToDB(){
-    
-}
+document.querySelector(".signUpButton").addEventListener("click", createFormToHTML() )
